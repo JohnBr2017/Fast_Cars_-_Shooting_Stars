8a40d9586305991ba349978f2c902416f416c188
John Broadhead <goomba2001.jb@gmail.com>
2018-02-19 16:45:37 -0700

    worked on scoreboard

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,86 +1,86 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
+import  Scoreboard  from "./scoreboard"
 
 
-// import filter from "./filter"
 import thunk from "redux-thunk";
 
 const store = createStore(
-    combineReducers({}),
+    combineReducers({ Scoreboard }),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(thunk)
 );
 
 export default store;
 
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class Redux extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputs: {
-                "playerOne": '',
-                "playerTwo": '',
-                "playerThree": '',
-                "playerFour": '',
-                "playerFive": '',
-                "players": ''
-            }
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleDelete = this.handleDelete.bind(this);
-        this.handleEdit = this.handleEdit.bind(this);
-    }
+// export default class Redux extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             inputs: {
+//                 "playerOne": '',
+//                 "playerTwo": '',
+//                 "playerThree": '',
+//                 "playerFour": '',
+//                 "playerFive": '',
+//                 "players": ''
+//             }
+//         }
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//         this.handleDelete = this.handleDelete.bind(this);
+//         this.handleEdit = this.handleEdit.bind(this);
+//     }
 
-    handleChange(e) {
+//     handleChange(e) {
 
-    }
+//     }
 
-    handleSubmit(e) {
+//     handleSubmit(e) {
 
-    }
+//     }
 
-    handleDelete(id) {
+//     handleDelete(id) {
 
-    }
+//     }
 
-    handleEdit(id, /*Something other*/) {
+//     handleEdit(id, /*Something other*/) {
 
-    }
+//     }
 
-    componentDidMount() {
-        axios.get(/*From our URL*/)
-            .then(response => {
-                this.setState({
-                    //players probably isnt what we wil call it, but its there just incase
-                    players: response.data,
-                    loading: false
-                })
-            })
-            .catch(err => {
-                console.error(err);
-            })
-    }
+//     componentDidMount() {
+//         axios.get(/*From our URL*/)
+//             .then(response => {
+//                 this.setState({
+//                     //players probably isnt what we wil call it, but its there just incase
+//                     players: response.data,
+//                     loading: false
+//                 })
+//             })
+//             .catch(err => {
+//                 console.error(err);
+//             })
+//     }
 
-    clearInputs() {
-        this.setState({
-            inputs: {
-                "playerOne": '',
-                "playerTwo": '',
-                "playerThree": '',
-                "playerFour": '',
-                "playerFive": '',
-                "players": ''
-            }
-        })
-    }
+//     clearInputs() {
+//         this.setState({
+//             inputs: {
+//                 "playerOne": '',
+//                 "playerTwo": '',
+//                 "playerThree": '',
+//                 "playerFour": '',
+//                 "playerFive": '',
+//                 "players": ''
+//             }
+//         })
+//     }
 
-    render() {
-        return (
-            <div>
+//     render() {
+//         return (
+//             <div>
 
-            </div>
-        )
-    }
-}
+//             </div>
+//         )
+//     }
+// }
--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const cors = require("cors");
+
 mongoose.connect("mongodb://localhost/modelz", (err) => {
     if (err) throw err;
     console.log("Connected to the Modelz Database");
 });
-
+app.use(cors());
 app.use(bodyParser.json());
-app.use('/modelz', require('./routes/modelz'));
+app.use('/modelz', require('./routes/routes'));
+app.use("/score", require("./routes/scoreboard"))
 
 
 app.listen(8080, () =>{
